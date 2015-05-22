7104652e13442641bcee584c59f73fad5ed5d50a
Giles Thomas <giles@giles.net>
2015-05-22 19:34:40 +0000

    REST API to get a list of words, and Angular stuff to display it.

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
+
+@app.route("/words/")
+def words_collection():
+    data = [
+        {
+            "id": "1",
+            "portuguese": "palavra 1",
+            "details": "The details for the first word",
+        },
+        {
+            "id": "2",
+            "portuguese": "palavra 2",
+            "details": "The details for the second word",
+        },
+        {
+            "id": "3",
+            "portuguese": "palavra 3",
+            "details": "The details for the third word",
+        }
+    ]
+    return Response(json.dumps(data),  mimetype='application/json')
